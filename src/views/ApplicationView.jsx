c062635cf30b881ff844f5b9926cd4e38c1bc35f
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-19 15:10:16 -0500

    Navbar & List views added

--- src/views/ApplicationView.jsx
+++ src/views/ApplicationView.jsx
@@ -1,13 +1,18 @@
-import {Route, Routes, Outlet} from "react-router-dom"
+import { Route, Routes, Outlet } from "react-router-dom";
+import { Navbar } from "../components/nav/Navbar";
 
 export const ApplicationViews = () => {
-    return <Routes>
-        <Route path="/" element={
-            <>
-                <Outlet />
-            </>
-        }>
-
-        </Route>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <Outlet />
+          </>
+        }
+      ></Route>
     </Routes>
-}
+  );
+};
