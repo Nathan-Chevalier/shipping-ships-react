758bcf4749f0875f2138dc8d60ec10a90cca1dc1
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-19 14:58:59 -0500

    Routes constructed

---
+++ src/views/ApplicationView.jsx
@@ -0,0 +1,13 @@
+import {Route, Routes, Outlet} from "react-router-dom"
+
+export const ApplicationViews = () => {
+    return <Routes>
+        <Route path="/" element={
+            <>
+                <Outlet />
+            </>
+        }>
+
+        </Route>
+    </Routes>
+}
