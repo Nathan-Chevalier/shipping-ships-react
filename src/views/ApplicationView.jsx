188ce25f3bb666d78bec9ca80c0132812e787d68
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-19 15:20:34 -0500

    Application Views built

--- src/views/ApplicationView.jsx
+++ src/views/ApplicationView.jsx
@@ -1,5 +1,9 @@
 import { Route, Routes, Outlet } from "react-router-dom";
 import { Navbar } from "../components/nav/Navbar";
+import { HaulerList } from "../components/lists/HaulerList";
+import { ShipList } from "../components/lists/ShipList";
+import { DockList } from "../components/lists/DockList";
+import { AllList } from "../components/lists/AllList";
 
 export const ApplicationViews = () => {
   return (
@@ -12,7 +16,14 @@ export const ApplicationViews = () => {
             <Outlet />
           </>
         }
-      ></Route>
+      >
+        <Route index element={<AllList />} />
+        <Route path="shippingships" element={<ShipList />} />
+        <Route path="haulingships" element={<HaulerList />}>
+          <Route path="edit" />
+        </Route>
+        <Route path="docks" element={<DockList />} />
+      </Route>
     </Routes>
   );
 };
