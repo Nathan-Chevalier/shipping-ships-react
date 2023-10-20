e9666bf0c7af054996daa744fb3243ed7572c7d9
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 09:37:16 -0500

    Expand added on hauler fetch

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,5 +1,5 @@
 export const getAllShips = () => {
-  return fetch(`http://localhost:8000/ships`).then((res) => res.json());
+  return fetch(`http://localhost:8000/ships?_expand=hauler`).then((res) => res.json());
 };
 
 export const getAllHaulers = () => {
