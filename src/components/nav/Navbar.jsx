303b5e5d3673ee4cfa699ba847bd96e209e11271
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 09:53:25 -0500

    Navbar created

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,3 +1,19 @@
+import { useNavigate, Link } from "react-router-dom";
+
 export const Navbar = () => {
-  return <>Hey Navbar</>;
+  return (
+    <div className="flex justify-center bg-slate-400 rounded">
+      <ul className="navbar flex justify-between items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
+        <li className="text-white">
+          <Link to="/ships">Ships</Link>
+        </li>
+        <li className="text-white">
+          <Link to="/haulers">Haulers</Link>
+        </li>
+        <li className="text-white">
+          <Link to="/docks">Docks</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
