0dd595a99689f771a0cf52ce7b2cdaf9d6f29a98
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 13:40:15 -0500

    JSX Structure built

--- src/components/lists/HaulerList.jsx
+++ src/components/lists/HaulerList.jsx
@@ -1,9 +1,11 @@
+import { useState, useEffect } from "react";
+
 export const HaulerList = () => {
   return (
     <div className="flex flex-col self-center bg-gradient-to-r from-slate-700/30 to-slate-400/30 rounded-3xl scale-auto w-max justify-center items-center mt-12">
-      <div className="ships-view-container flex flex-col items-center justify-center h-2/6 my-4">
-        <div className="ships-view-header"></div>
-        <div className="ships-view-list">{}</div>
+      <div className="haulers-view-container flex flex-col items-center justify-center h-2/6 my-4">
+        <div className="haulers-view-header"></div>
+        <div className="haulers-view-list">{}</div>
       </div>
     </div>
   );
