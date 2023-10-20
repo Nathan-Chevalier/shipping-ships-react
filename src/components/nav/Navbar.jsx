d9c78b95bce32fafd3c411c0fed30c67369390b8
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 10:00:01 -0500

    Navbar functioning

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div className="flex justify-center bg-slate-400 rounded">
-      <ul className="navbar flex justify-between items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
+      <ul className="flex justify-around items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
         <li className="text-white">
           <Link to="/shippingships">Ships</Link>
         </li>
