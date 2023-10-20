d7b499cb4ffb984d472af874caea4877468602cd
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 13:31:02 -0500

    Landing view styled

--- src/components/lists/AllList.jsx
+++ src/components/lists/AllList.jsx
@@ -7,15 +7,23 @@ import {
 
 export const AllList = () => {
   const [ships, setShips] = useState([]);
+  const [haulers, setHaulers] = useState([]);
+  const [docks, setDocks] = useState([]);
 
   useEffect(() => {
     getAllShips().then((shipsArray) => {
       setShips(shipsArray);
     });
+    getAllHaulers().then((haulersArray) => {
+      setHaulers(haulersArray);
+    });
+    getAllDocks().then((docksArray) => {
+      setDocks(docksArray);
+    });
   }, []);
 
   return (
-    <div className="flex flex-col bg-gradient-to-r from-slate-700 to-slate-400 rounded-3xl">
+    <div className="flex flex-col self-center bg-gradient-to-r from-slate-700/30 to-slate-400/30 rounded-3xl scale-auto w-max justify-center items-center mt-12">
       <div className="ships-container flex flex-col items-center justify-center h-2/6 my-4">
         <div className="title-header text-white text-3xl bg-cyan-900 py-4 px-8 rounded-t-2xl border-cyan-200 border-4">
           Shipping Ships
@@ -25,9 +33,43 @@ export const AllList = () => {
             return (
               <div
                 key={ship.id}
-                className="list-item bg-cyan-800 rounded-xl py-2 px-4 m-2"
+                className="bg-cyan-800 rounded-xl py-2 px-4 m-2"
               >
                 <span>{ship.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="haulers-container flex flex-col items-center justify-center h-2/6 my-4">
+        <div className="title-header text-white text-3xl bg-sky-900 py-4 px-8 rounded-t-2xl border-sky-200 border-4">
+          Hauling Haulers
+        </div>
+        <div className="ships-list text-white columns-4 bg-sky-500/70 border-sky-200 border-4 p-5 rounded-3xl">
+          {haulers.map((hauler) => {
+            return (
+              <div
+                key={hauler.id}
+                className=" bg-sky-800 rounded-xl py-2 px-4 m-2"
+              >
+                <span>{hauler.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="docks-container flex flex-col items-center justify-center h-2/6 my-4">
+        <div className="title-header text-white text-3xl bg-blue-900 py-4 px-8 rounded-t-2xl border-blue-200 border-4">
+          Docking Docks
+        </div>
+        <div className="ships-list text-white columns-4 bg-blue-500/70 border-blue-200 border-4 p-5 rounded-3xl">
+          {docks.map((dock) => {
+            return (
+              <div
+                key={dock.id}
+                className="bg-blue-800 rounded-xl py-2 px-4 m-2"
+              >
+                <span>{dock.location}</span>
               </div>
             );
           })}
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -5,13 +5,15 @@ export const Navbar = () => {
     <div className="flex justify-center bg-slate-400 rounded">
       <ul className="flex justify-around items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
         <li className="text-white">
-          <Link to="/shippingships">Ships</Link>
+          <Link className="btn-save" to="/shippingships">
+            Ships
+          </Link>
         </li>
         <li className="text-white">
-          <Link to="/haulingships">Haulers</Link>
+          <Link className="btn-save"  to="/haulingships">Haulers</Link>
         </li>
         <li className="text-white">
-          <Link to="/docks">Docks</Link>
+          <Link className="btn-save"  to="/docks">Docks</Link>
         </li>
       </ul>
     </div>
