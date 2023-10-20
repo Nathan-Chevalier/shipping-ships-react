83ad233185ea3397f85f5e45bc28c46834c7a2f0
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-20 09:57:34 -0500

    Links corrected

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,14 +1,14 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div className="flex justify-center bg-slate-400 rounded">
       <ul className="navbar flex justify-between items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
         <li className="text-white">
-          <Link to="/ships">Ships</Link>
+          <Link to="/shippingships">Ships</Link>
         </li>
         <li className="text-white">
-          <Link to="/haulers">Haulers</Link>
+          <Link to="/haulingships">Haulers</Link>
         </li>
         <li className="text-white">
           <Link to="/docks">Docks</Link>
