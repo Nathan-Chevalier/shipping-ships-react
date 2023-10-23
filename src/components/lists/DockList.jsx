17e1fa420e60123bc9656f50dbb15cfe64ecdec6
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-23 15:32:24 -0500

    Dock view functioning

--- src/components/lists/DockList.jsx
+++ src/components/lists/DockList.jsx
@@ -1,3 +1,41 @@
+import { useState, useEffect } from "react";
+import { getAllDocks } from "../../services/fetchService";
+
 export const DockList = () => {
-  return <>Hey Docks</>;
+  const [docks, setDocks] = useState([]);
+
+  useEffect(() => {
+    getAllDocks().then((docksArray) => {
+      setDocks(docksArray);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col self-center">
+      <div className="dock-view-container">
+        <div className="dock-view-header"></div>
+        <div className="dock-view-list">
+          {docks.map((dock) => {
+            return (
+              <div key={dock.id} className="dock-row flex-col">
+                <div className="dock-info">
+                  <span>{dock?.location}</span>
+                  <span>{dock?.capacity}</span>
+                </div>
+                <div className="haulers-list">
+                  {dock?.haulers.map((hauler) => {
+                    return (
+                      <div key={hauler.id} className="hauler-row flex">
+                        <span className="list-item">{hauler.name}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
