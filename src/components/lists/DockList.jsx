a1ca2a4d384e448d5cf1a05b38bbf535da0fb692
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-24 09:19:18 -0500

    Full list functionality, basic styling applied

--- src/components/lists/DockList.jsx
+++ src/components/lists/DockList.jsx
@@ -11,26 +11,26 @@ export const DockList = () => {
   }, []);
 
   return (
-    <div className="flex flex-col self-center">
-      <div className="dock-view-container">
+    <div className="flex flex-col self-center bg-gradient-to-r from-slate-700/30 to-slate-400/30 rounded-3xl scale-auto w-max justify-center items-center mt-12">
+      <div className="dock-view-container flex flex-col items-center justify-center my-4">
         <div className="dock-view-header"></div>
-        <div className="dock-view-list">
+        <div className="dock-view-list flex flex-col gap-2">
           {docks.map((dock) => {
             return (
               <div key={dock.id} className="dock-row flex-col">
-                <div className="dock-info">
-                  <span>{dock?.location}</span>
-                  <span>{dock?.capacity}</span>
+                <div className="dock-info flex justify-between items-center bg-cyan-900/90 px-12 py-2 mx-12 rounded-lg">
+                  <span className="bg-fade">{dock?.location}</span>
+                  <span className="bg-fade">{dock?.capacity}</span>
                 </div>
-                <div className="haulers-list">
+                <ul className="haulers-list">
                   {dock?.haulers.map((hauler) => {
                     return (
-                      <div key={hauler.id} className="hauler-row flex">
-                        <span className="list-item">{hauler.name}</span>
-                      </div>
+                      <li key={hauler.id} className="hauler-row flex">
+                        <span className="list-item bg-fade">{hauler.name}</span>
+                      </li>
                     );
                   })}
-                </div>
+                </ul>
               </div>
             );
           })}
